b4041330ed9e987dbb536b192563d9d0b2cddf72
NanguRepo <nanguthenangu@gmail.com>
2022-03-21 08:28:24 +0100

    hash routing!!

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -24,7 +24,7 @@ export default function Header(props) {
                   ['GitHub', '/github']
                 ].map(([title, url]) => (
                   <li key={title}>
-                    <a href={url} key={title} className={`navbar-item text-black dark:text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${title === props.page ? 'font-bold underline': 'hover-underline-animation'}`} aria-current="page">{title}</a>
+                    <a href={`/#${url}`} key={title} className={`navbar-item text-black dark:text-white hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${title === props.page ? 'font-bold underline': 'hover-underline-animation'}`} aria-current="page">{title}</a>
                   </li>
                 ))}
               </ul>
--- app/src/pages/home.js
+++ app/src/pages/home.js
@@ -14,9 +14,9 @@ export default function Home() {
         <Hero title="NanguRepo" subtitle="Web developer, React.js learner and API maintainer." />
         <div className="mt-10" />
         <div className="columns-3">
-          <div><Button href='/projects' text="My projects" icon={<FaBoxOpen size="100" />} className="text-4xl font-sans h-full w-full" /></div>
-          <div><Button href='/contact' text="Contact me" icon={<FaDiscord size="100" />} className="text-4xl font-sans h-full w-full" /></div>
-          <div><Button href='/github' text="Visit my GitHub" icon={<FaGithub size="100" />} className="text-4xl font-sans h-full w-full" /></div>
+          <div><Button href='/#/projects' text="My projects" icon={<FaBoxOpen size="100" />} className="text-4xl font-sans h-full w-full" /></div>
+          <div><Button href='/#/contact' text="Contact me" icon={<FaDiscord size="100" />} className="text-4xl font-sans h-full w-full" /></div>
+          <div><Button href='/#/github' text="Visit my GitHub" icon={<FaGithub size="100" />} className="text-4xl font-sans h-full w-full" /></div>
         </div>
       </div>
       <Footer />
